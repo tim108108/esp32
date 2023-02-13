66b7bb648b23c27c3973de64ce3e99aff3553d46
tim108108 <tim108108@gmail.com>
2023-02-14 01:13:01 +0800

    0109

--- 2.Software/code.c
+++ 2.Software/code.c
@@ -1,29 +1,35 @@
-const int ledCount = 10;    // 總共有幾顆燈
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int ledPins[] = {
-  16, 17, 5, 18, 19, 21, 3, 1, 22, 23
-};   // 使用一陣列來記錄所使用的GPIOs
-
-
-void setup() {
-  // 用一個for迴圈來設定所有的LED模式
-  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
-    pinMode(ledPins[thisLed], OUTPUT);
-  }
+//1 pulse=1.8 degree
+void drive(int deg, bool side, bool opt){
+    int s[4],wave_table[] = {1,0,0,0};
+    deg/=1.8;
+    if(opt){
+      wave_table[3] = 1;
+    }
+    if (side){
+    for (int i=0;i<deg;i++){
+      s[0]=wave_table[(i+4)%4];
+      s[1]=wave_table[(i+3)%4];
+      s[2]=wave_table[(i+2)%4];
+      s[3]=wave_table[(i+1)%4];
+      printf("%d",s[0]);printf("%d",s[1]);printf("%d",s[2]);printf("%d\n",s[3]);
+    }
+    }
+    else{
+    for (int i=0;i<deg;i++){
+      s[0]=wave_table[(i+1)%4];
+      s[1]=wave_table[(i+2)%4];
+      s[2]=wave_table[(i+3)%4];
+      s[3]=wave_table[(i+4)%4];
+      printf("%d",s[0]);printf("%d",s[1]);printf("%d",s[2]);printf("%d\n",s[3]);
+    }
+    }
 }
 
-void loop() {
-    for (int thisLed = 0; thisLed < ledCount; thisLed++){
-      digitalWrite(ledPins[thisLed], HIGH);
-      delay(50);
-      digitalWrite(ledPins[thisLed], LOW);
-      delay(50);
-    }
-      for (int thisLed = ledCount-1; thisLed >= 0; thisLed--){
-      digitalWrite(ledPins[thisLed], HIGH);
-      delay(50);
-      digitalWrite(ledPins[thisLed], LOW);
-      delay(50);
-    }
-
+int main(){
+  drive(36,true,true);
+  printf("\n");
 }
